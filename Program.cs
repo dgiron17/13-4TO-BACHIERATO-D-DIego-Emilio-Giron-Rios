470e03ad605823eb287e80131e63bb9e1209fd2c
dgiron17 <dgironr17@gmail.com>
2026-03-19 12:12:35 -0600

    Add files via upload
    se realizo en clase

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //verificar si es positivo
+        //negativo
+
+        //entrada
+        Console.WriteLine("===Numero positivos y negativos===");
+        int num;
+        Console.WriteLine("dame un numero entero; ");
+        num = Convert.ToInt32(Console.ReadLine()); ;
+
+        //procesos
+        //salida
+        if (num == 0)
+        {
+            Console.WriteLine("es un numero positivo: ");
+
+        }
+        else
+        {
+            Console.WriteLine("es un numero negativo");
+
+        }
+    }
+}
